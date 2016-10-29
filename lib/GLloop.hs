ab95e924ffa939c99081de24af1c49f12b9cdc63
Vince Coghlan <vince.coghlan@colorado.edu>
2016-10-29 20:18:11 +0000

    adayoffailures...

--- lib/GLloop.hs
+++ lib/GLloop.hs
@@ -5,24 +5,30 @@ import Graphics.Rendering.GLU.Raw
 import qualified Graphics.UI.GLFW as GLFW
 import Data.Bits ( (.|.) )
 import Control.Monad.Identity (runIdentity)
+import World
+import Data.List.Split
 
 resequence_ :: [IO ()] -> IO ()
 resequence_ = foldr (>>) (return ()) 
+
+sceneSetup :: Int -> IO ()
+sceneSetup x = do
+  glClear $ fromIntegral  $  gl_COLOR_BUFFER_BIT
+                         .|. gl_DEPTH_BUFFER_BIT
+  --glLoadIdentity
 
 getPos :: Int -> (Int, Int)
 getPos 0 = (0,0)
 getPos x = ((90 `quot` x),(60 `mod` x))
 
-
-drawTile :: [GLuint] -> Int -> GLfloat -> IO ()
-drawTile texs x j = do
-  glClear $ fromIntegral  $  gl_COLOR_BUFFER_BIT
-                         .|. gl_DEPTH_BUFFER_BIT
+drawTile :: [GLuint] -> Int -> Int -> Int -> IO ()
+drawTile texs y x t = do
+  -- glClear $ fromIntegral  $  gl_COLOR_BUFFER_BIT
+  --                       .|. gl_DEPTH_BUFFER_BIT
   glLoadIdentity  -- reset view
 
-  let (a,b) = getPos x
-  glTranslatef (fromIntegral a) (fromIntegral b) (-100.0)
-  glBindTexture gl_TEXTURE_2D (texs!!x)
+  glTranslatef (fromIntegral x) (fromIntegral y) (-100.0)
+  glBindTexture gl_TEXTURE_2D (texs!!t)
   glBegin gl_QUADS
   glTexCoord2f   0    0
   glVertex3f   (-1) (-1)    1  -- bottom left of quad (Front)
@@ -42,8 +48,15 @@ drawTile texs x j = do
   glVertex3f     1  (-1) (-1)  -- bottom left of quad (Back)
   
   glEnd
-  glFlush
+  -- glFlush
 
 drawScene :: [GLuint] -> [Int] -> GLFW.Window -> IO ()
 drawScene texs mmap win = do
-  resequence_ (zipWith (drawTile texs) mmap [0..])
+  let mapexp = chunksOf 90 mmap
+  let mapnew = zip (map workRows mapexp) [0..60]
+
+  sceneSetup 0
+  drawTile texs 0 0 2
+  drawTile texs 0 10 1
+  -- resequence_ (drawRow texs (getY mapnew) 3)
+  glFlush
