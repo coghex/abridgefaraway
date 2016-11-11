398f982d5357d0358ceb2faa9df82b6f2d7c551a
Vince Coghlan <vince.coghlan@colorado.edu>
2016-11-11 08:00:47 -0700

    more zone functions
    Former-commit-id: 16beed760e7cf6f1b09f3ef85fe6cae18efa9489

--- lib/Zone.hs
+++ lib/Zone.hs
@@ -1,5 +1,6 @@
 module Zone where
 
+import Data.List.Split (chunksOf)
 import State
 import Settings
 import World
@@ -23,5 +24,30 @@ buildZone state x y = do
 
 changeZone :: Int -> ([Int], Int) -> ([Int], Int)
 changeZone i (a, j)
-  | (i==j)    = ((take (256*256) (repeat 1)), j)
+  | (i==j)    = ((buildThisZone a), j)
   | otherwise = (a, j)
+
+buildThisZone :: [Int] -> [Int]
+buildThisZone l = do
+  let z0 = expandThisZone (take (256*256) (repeat 0))--l
+      z1 = stripZone z0
+      z2 = flattenZone z1
+  z2
+
+expandThisZone :: [Int] -> [([(Int, Int)], Int)]
+expandThisZone m = zip (map workZoneRows (chunksOf zonew m)) [0..zoneh]
+
+workZoneRows :: [Int] -> [(Int, Int)]
+workZoneRows l = do
+  zip l [0..zonew]
+
+stripZone :: [([(Int, Int)], Int)] -> [[Int]]
+stripZone ((a,b):ys) = (stripZoneRow a) : stripZone ys
+stripZone _          = [[]]
+
+stripZoneRow :: [(Int, Int)] -> [Int]
+stripZoneRow ((a,b):ys) = a : stripZoneRow ys
+stripZoneRow _          = []
+
+flattenZone :: [[Int]] -> [Int]
+flattenZone xs = (\z n -> foldr (\x y -> foldr z y x) n xs) (:) []
