75466be675a2d5a63330c2ee0ad8d5c2febaa89b
Vince Coghlan <vince.coghlan@colorado.edu>
2016-11-17 10:32:49 -0700

    beginning functional fanatics
    Former-commit-id: a573404ce43226eaa2c5c40074318f461d646c25

--- src/Game/Road.hs
+++ src/Game/Road.hs
@@ -4,19 +4,27 @@ import Game.Zone
 
 initPath :: Int -> Int -> Int -> Int -> [Int] -> [Int] -> [Int]
 initPath x y dir t z []        = z
-initPath x y 0 0 z (2:rands)   = do
+initPath x y 0   0 z (2:rands) = do
   let z0 = expandZone z
   let z1 = map (pathRow x y 2) z0
   let z2 = stripZone z1
   let z3 = flattenZone z2
   let z4 = initPath x (y+1) 3 2 z3 rands
   initPath x (y-1) 1 2 z4 rands
-initPath x y dir t z (r:rands) = do
+initPath x y 1   2 z (2:rands) = do
   let z0 = expandZone z
-  let z1 = map (pathRow x y t) z0
+  let z1 = map (pathRow x y 2) z0
   let z2 = stripZone z1
   let z3 = flattenZone z2
-  initPath x y 0 t z3 rands
+  initPath x (y-1) 1 2 z3 rands
+initPath x y 3   2 z (2:rands) = do
+  let z0 = expandZone z
+  let z1 = map (pathRow x y 2) z0
+  let z2 = stripZone z1
+  let z3 = flattenZone z2
+  initPath x (y+1) 3 2 z3 rands
+initPath x y dir t z (r:rands) = do
+  initPath x y dir t z rands
 
 pathRow :: Int -> Int -> Int -> ([(Int, Int)], Int) -> ([(Int, Int)], Int)
 pathRow x y t (a, b) = ((map (pathSpot x y t b) a), b)
