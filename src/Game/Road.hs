47b48178002e834bd68cefcdcd130691d99f6166
Vince Coghlan <vince.coghlan@colorado.edu>
2016-11-18 09:07:48 -0700

    still not quite sure how this works
    Former-commit-id: 240f75f38e580989b638082f1258cece3788ce40

--- src/Game/Road.hs
+++ src/Game/Road.hs
@@ -2,6 +2,14 @@ module Game.Road where
 
 import Game.Zone
 
+nfits1 = [2]
+sfits2 = [1, 2, 6]
+nfits2 = [2]
+nfits6 = [2]
+efits6 = [7]
+wfits7 = [6, 7]
+efits7 = [7]
+
 setZ :: Int -> Int -> Int -> [Int] -> [Int]
 setZ n x y z = do
   let z0 = expandZone z
@@ -11,19 +19,11 @@ setZ n x y z = do
 
 initPath :: Int -> Int -> Int -> Int -> [Int] -> [Int] -> [Int]
 initPath x y dir t z []        = z
+--------------------------------------------------------------
 -- paths2
 --------------------------------------------------------------
 initPath x y 0   0 z (1:rands) = initPath x (y+1) 3 1 (setZ 1 x y z) rands
-initPath x y 1   2 z (1:rands) = setZ 1 x y z
-initPath x y 1   3 z (1:rands) = setZ 1 x y z
-initPath x y 1  19 z (1:rands) = setZ 1 x y z
-initPath x y 1  21 z (1:rands) = setZ 1 x y z
-initPath x y 1  22 z (1:rands) = setZ 1 x y z
-initPath x y 1  25 z (1:rands) = setZ 1 x y z
-initPath x y 1  33 z (1:rands) = setZ 1 x y z
-initPath x y 1  34 z (1:rands) = setZ 1 x y z
-initPath x y 1  36 z (1:rands) = setZ 1 x y z
-initPath x y 1  38 z (1:rands) = setZ 1 x y z
+initPath x y 1   n z (1:rands) = if (n `elem` nfits1) then (setZ 1 x y z) else (initPath x y 1 n z rands)
 --------------------------------------------------------------
 -- paths3
 --------------------------------------------------------------
@@ -31,170 +31,52 @@ initPath x y 0   0 z (2:rands) = do
   let z0 = setZ 2 x y z
   let z1 = initPath x (y+1) 3 2 z0 rands
   initPath x (y-1) 1 2 z1 rands
------------------------------------------
-initPath x y 3   1 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y+1) 3 2 z0 rands
------------------------------------------
-initPath x y 1   2 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y-1) 1 2 z0 rands
-initPath x y 3   2 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y+1) 3 2 z0 rands
------------------------------------------
-initPath x y 1   3 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y-1) 1 2 z0 rands
------------------------------------------
-initPath x y 3   6 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y+1) 3 2 z0 rands
------------------------------------------
-initPath x y 3   8 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y+1) 3 2 z0 rands
------------------------------------------
-initPath x y 3   9 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y+1) 3 2 z0 rands
------------------------------------------
-initPath x y 1  16 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y-1) 1 2 z0 rands
------------------------------------------
-initPath x y 1  19 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y-1) 1 2 z0 rands
------------------------------------------
-initPath x y 1  21 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y-1) 1 2 z0 rands
-initPath x y 3   21 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y+1) 3 2 z0 rands
------------------------------------------
-initPath x y 1  22 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y-1) 1 2 z0 rands
-initPath x y 3  22 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y+1) 3 2 z0 rands
------------------------------------------
-initPath x y 1  27 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y-1) 1 2 z0 rands
------------------------------------------
-initPath x y 1  33 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y-1) 1 2 z0 rands
------------------------------------------
-initPath x y 1  34 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y-1) 1 2 z0 rands
------------------------------------------
-initPath x y 1  36 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y-1) 1 2 z0 rands
------------------------------------------
-initPath x y 1  38 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y-1) 1 2 z0 rands
------------------------------------------
-initPath x y 3  40 z (2:rands) = do
-  let z0 = setZ 2 x y z
-  initPath x (y+1) 3 2 z0 rands
+initPath x y 3   n z (2:rands) = if (n `elem` sfits2) then do
+    let z0 = setZ 2 x y z
+    initPath x (y+1) 3 2 z0 rands
+  else
+    initPath x y 3 n z rands
+initPath x y 1   n z (2:rands) = if (n `elem` nfits2) then do
+    let z0 = setZ 2 x y z
+    initPath x (y-1) 1 2 z0 rands
+  else
+    initPath x y 1 n z rands
 --------------------------------------------------------------
--- paths4
+-- paths7
 --------------------------------------------------------------
-initPath x y 0   0 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  let z2 = initPath x (y-1) 1 3 z1 rands
-  initPath (x+1) y 4 3 z2 rands
------------------------------------------
-initPath x y 3   1 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  initPath (x+1) y 4 3 z1 rands
------------------------------------------
-initPath x y 3   2 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  initPath (x+1) y 4 3 z1 rands
------------------------------------------
-initPath x y 2   4 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  initPath x (y-1) 1 3 z1 rands
------------------------------------------
-initPath x y 3   6 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  initPath (x+1) y 4 3 z1 rands
------------------------------------------
-initPath x y 3   8 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  initPath (x+1) y 4 3 z1 rands
------------------------------------------
-initPath x y 3   9 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  initPath (x+1) y 4 3 z1 rands
-initPath x y 1   9 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y-1) 1 3 z0 rands
-  initPath (x+1) y 4 3 z1 rands
------------------------------------------
-initPath x y 2  13 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  initPath x (y-1) 1 3 z1 rands
------------------------------------------
-initPath x y 2  16 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  initPath x (y-1) 1 3 z1 rands
------------------------------------------
-initPath x y 3  21 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  initPath (x+1) y 4 3 z1 rands
------------------------------------------
-initPath x y 3  22 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  initPath (x+1) y 4 3 z1 rands
------------------------------------------
-initPath x y 1  23 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y-1) 1 3 z0 rands
-  initPath (x+1) y 4 3 z1 rands
------------------------------------------
-initPath x y 3  33 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y+1) 3 3 z0 rands
-  initPath (x+1) y 4 3 z1 rands
------------------------------------------
-initPath x y 1  39 z (3:rands) = do
-  let z0 = setZ 3 x y z
-  let z1 = initPath x (y-1) 1 3 z0 rands
-  initPath (x+1) y 4 3 z1 rands
----------------------------------------------------------------
--- paths5
----------------------------------------------------------------
-initPath x y 0   0 z (4:rands) = do
-  let z0 = setZ 4 x y z
-  let z1 = initPath (x-1) y 2 4 z0 rands
-  initPath x (y+1) 3 4 z1 rands
-initPath x y 4   3 z (4:rands) = do
-  let z0 = setZ 4 x y z
-  initPath (x-1) y 2 4 z0 rands
-initPath x y 1  10 z (4:rands) = do
-  let z0 = setZ 4 x y z
-  initPath x (y+1) 3 4 z0 rands
--------------------------------------------------------------------
+initPath x y 0   0 z (6:rands) = do
+  let z0 = setZ 6 x y z
+  let z1 = initPath x (y+1) 3 6 z0 rands
+  initPath (x+1) y 4 6 z1 rands
+initPath x y 2   n z (6:rands) = if (n `elem` efits6) then do
+    let z0 = setZ 6 x y z
+    initPath x (y+1) 3 6 z0 rands
+  else
+    initPath x y 2 n z rands
+initPath x y 1   n z (6:rands) = if (n `elem` nfits6) then do
+    let z0 = setZ 6 x y z
+    initPath (x+1) y 4 6 z0 rands
+  else
+    initPath x y 2 n z rands
+--------------------------------------------------------------
+-- paths8
+--------------------------------------------------------------
+initPath x y 0   0 z (7:rands) = do
+  let z0 = setZ 7 x y z
+  let z1 = initPath (x-1) y 2 7 z0 rands
+  initPath (x+1) y 4 7 z1 rands
+initPath x y 2   n z (7:rands) = if (n `elem` efits7) then do
+    let z0 = setZ 7 x y z
+    initPath (x-1) y 2 7 z0 rands
+  else
+    initPath x y 2 n z rands
+initPath x y 4   n z (7:rands) = if (n `elem` wfits7) then do
+    let z0 = setZ 7 x y z
+    initPath (x+1) y 4 7 z0 rands
+  else
+    initPath x y 2 n z rands
+
+------------------------------------------------------------------------
 initPath x y dir t z (r:rands) = do
   initPath x y dir t z rands
 
