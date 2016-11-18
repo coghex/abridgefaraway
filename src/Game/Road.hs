edbb3a3b7f5d1d4a25c05425860e060a365b9041
Vince Coghlan <vince.coghlan@colorado.edu>
2016-11-18 12:22:14 -0700

    more paths
    Former-commit-id: 6542dadd02edef629f76f0bb88bf4da1c48ac90e

--- src/Game/Road.hs
+++ src/Game/Road.hs
@@ -2,13 +2,21 @@ module Game.Road where
 
 import Game.Zone
 
-nfits1 = [2]
-sfits2 = [1, 2, 6]
-nfits2 = [2]
-nfits6 = [2]
-efits6 = [7]
-wfits7 = [6, 7]
-efits7 = [7]
+south = 1::Int
+west = 2::Int
+north = 3::Int
+east = 4::Int
+nfits1 = [2, 19]
+sfits2 = [1, 2, 6, 8]
+nfits2 = [2, 19]
+nfits6 = [2, 19]
+efits6 = [7, 8]
+wfits7 = [6, 7, 14]
+efits7 = [7, 8]
+wfits8 = [6, 7, 14]
+nfits8 = [2, 19]
+efits14 = [7, 8]
+sfits19 = [1, 2, 6, 8]
 
 setZ :: Int -> Int -> Int -> [Int] -> [Int]
 setZ n x y z = do
@@ -75,7 +83,43 @@ initPath x y 4   n z (7:rands) = if (n `elem` wfits7) then do
     initPath (x+1) y 4 7 z0 rands
   else
     initPath x y 2 n z rands
-
+--------------------------------------------------------------
+-- paths9
+--------------------------------------------------------------
+initPath x y 0   0 z (8:rands) = do
+  let z0 = setZ 8 x y z
+  let z1 = initPath (x-1) y 2 8 z0 rands
+  initPath x (y+1) 3 8 z0 rands
+initPath x y 4   n z (8:rands) = if (n `elem` wfits8) then do
+    let z0 = setZ 8 x y z
+    initPath x (y+1) 3 8 z0 rands
+  else
+    initPath x y 4 n z rands
+initPath x y 1   n z (8:rands) = if (n `elem` nfits8) then do
+    let z0 = setZ 8 x y z
+    initPath (x-1) y 2 8 z0 rands
+  else
+    initPath x y 1 n z rands
+--------------------------------------------------------------
+-- paths15
+--------------------------------------------------------------
+initPath x y 0   0 z (14:rands) = do
+  let z0 = setZ 14 x y z
+  initPath (x+1) y 4 14 z0 rands
+initPath x y 2   n z (14:rands) = if (n `elem` efits14) then do
+    setZ 14 x y z
+  else
+    initPath x y 2 n z rands
+--------------------------------------------------------------
+-- paths20
+--------------------------------------------------------------
+initPath x y 0   0 z (19:rands) = do
+  let z0 = setZ 19 x y z
+  initPath x (y-1) 1 19 z0 rands
+initPath x y 3   n z (19:rands) = if (n `elem` sfits19) then do
+    setZ 19 x y z
+  else
+    initPath x y 2 n z rands
 ------------------------------------------------------------------------
 initPath x y dir t z (r:rands) = do
   initPath x y dir t z rands
