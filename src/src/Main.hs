ca06f5629950c03ca5df1725e41d823bfa6af9b6
coghex <vincecoghlan@gmail.com>
2020-07-26 23:22:57 +0000

    loads 2 textures, indexes in the frag

--- src/src/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
--- where the magic happens...
-import Artos
-import Paracletus
-import Paracletus.Data
-import Anamnesis.Util
--- runs a monadic action in IO
-main ∷ IO ()
-main = runAnamnesis checkStatus (runParacletus Vulkan)
