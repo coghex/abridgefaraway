65aeb8a578ddbd699dc32c161dc50616702824c1
coghex <vincecoghlan@gmail.com>
2020-10-23 11:58:55 -0700

    some menuing examples

--- mod/base/base.lua
+++ mod/base/base.lua
@@ -15,12 +15,15 @@ end
 function initLua ()
     newWindow ( "menu", "dat/tex/texture1.png" )
     newWindow ( "createParams", "dat/tex/black.png" )
+    newWindow ( "game", "dat/tex/black.png" )
     newText ("menu", -4.0, 4.0, "A Bridge Far Away...")
     newButtonAction ("menu", -4.0, 2.0, "Create World", "link", "createParams")
+    newButtonAction ("createParams", -4.0, -2.0, "Create World", "link", "game")
+    newButtonAction ("createParams", -4.0, -4.0, "back", "link", "menu")
     newButton ("menu", -4.0, 0.0, "Load World")
     newButtonAction ("menu", -4.0, -2.0, "Escape", "action", "quit")
-    newTile ( "createParams", 0.0, 0.0, "dat/tex/texture2.png" )
-    newTile ( "createParams", 1.0, 1.0, "dat/tex/texture1.png" )
+    newTile ( "game", 0.0, 0.0, "dat/tex/texture2.png" )
+    newTile ( "game", 1.0, 1.0, "dat/tex/texture1.png" )
     switchWindow ( "menu" )
     return 1
 end
